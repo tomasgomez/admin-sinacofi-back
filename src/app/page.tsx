99c27eae7e5d80637b503243c788728f258f7ca6
Erick Gomez <erick.gomez@leniolabs.com>
2024-03-18 10:31:50 -0300

    feat: add users section

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Hello World
+      </div>
+    </div>
+  );
+};
